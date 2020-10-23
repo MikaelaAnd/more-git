ed541daca13be6bee264319b9d4950e674746de1
Mikaela Andersson <mike@Mikaelas-MacBook-Pro.local>
2020-10-23 14:45:46 +0200

    add JSdoc comments

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
+
+/**
+ * Will generate a random number and print it into the DOM
+ */
 function printRandomNumber() {
     const randomNumber = getRandomNumber();
     addNumberToPage(randomNumber)
 }
 
-
+/**
+ * Generates random number and return it.
+ * @returns {number} the random number that was generated
+ */
 function getRandomNumber () {
     const randomNumber = Math.random() * 100;
     return Math.round(randomNumber);
 }
-
+/**
+ * 
+ * @param {number} number 
+ */
 function addNumberToPage(number) {
     const div = document.getElementById('results')
     div.append(number + ' , ');
