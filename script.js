e258a4fb42b91fd262231e3c9895c465b5dcd000
Mikaela Andersson <mike@Mikaelas-MacBook-Pro.local>
2020-10-23 13:53:28 +0200

    add functions

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-function printRandomColor() {
-    console.log('test');
+function printRandomNumber() {
+    const randomNumber = getRandomNumber();
+    addNumberToPage(randomNumber)
+}
+
+
+function getRandomNumber () {
+    const randomNumber = Math.random() * 100;
+    return Math.round(randomNumber);
+}
+
+function addNumberToPage(number) {
+    console.log(number);
 }
